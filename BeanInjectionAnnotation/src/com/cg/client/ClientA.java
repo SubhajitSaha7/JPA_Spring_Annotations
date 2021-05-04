36cfda3c4b0fc851a24dd80372dd54839afeb20f
SubhajitSaha7 <81805272+SubhajitSaha7@users.noreply.github.com>
2021-05-04 12:32:49 +0530

    Add files via upload

---
+++ BeanInjectionAnnotation/src/com/cg/client/ClientA.java
@@ -0,0 +1,21 @@
+package com.cg.client;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.cg.service.Service;
+
+public class ClientA {
+
+	static ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+	
+	public static void main(String[] args) {
+		System.out.println("main satrts");
+		Service ser = ctx.getBean("myservice", Service.class);
+		System.out.println(ser.viewEmployee()); 
+		System.out.println("demand second time");
+		Service ser2 = ctx.getBean("myservice", Service.class);
+		System.out.println(ser2.viewEmployee()); 
+	}
+
+}
